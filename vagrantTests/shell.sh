2436a212665dbf47b837210b196b6d38007d4954
AshIsbitt <ashisbitt@icloud.com>
2021-10-29 17:04:03 +0100

    vagrantfile works now!  will now create a new VM with py310 installed and the password changed, and will no longer open the virtualboxVM UI

--- vagrantTests/shell.sh
+++ vagrantTests/shell.sh
@@ -2,10 +2,11 @@
 
 # Change root password
 echo "Changing root password"
-echo "q" | passwd root
+echo "vagrant:q" | chpasswd
 
 # Add deadsnakes-PPA and install py310
 echo "Installing py310"
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.10
+sudo apt install -y  python3.10
 
+echo "HELLO HELLO HELLO HELLO HELLO HELLO HELLO"
